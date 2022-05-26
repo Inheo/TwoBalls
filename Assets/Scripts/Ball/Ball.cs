d631887b8fc7b422b34b5731070334e43b5db648
Inheo <agch777505@gmail.com>
2022-05-26 14:35:38 +0300

    added ripple ball

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 [RequireComponent(typeof(Mover))]
@@ -9,6 +10,9 @@ public class Ball : MonoBehaviour
 
     private Mover _mover;
     private Finish _currentFinish;
+
+    private Vector3 _startScale;
+    private Coroutine _ripple;
 
     public event System.Action OnFinished;
     public event System.Action OnFail;
@@ -23,17 +27,13 @@ public class Ball : MonoBehaviour
         _mover.OnMoveStart += ResetFinish;
 
         _trailRenderer.enabled = false;
+
+        _startScale = transform.localScale;
     }
 
     private void OnDestroy()
     {
         _mover.OnMoveStart -= ResetFinish;
-    }
-
-    private void ResetFinish()
-    {
-        _currentFinish = null;
-        IsFinished = false;
     }
 
     private void LateUpdate()
@@ -52,7 +52,7 @@ public class Ball : MonoBehaviour
     {
         if (other.TryGetComponent(out Finish finish))
         {
-            _currentFinish = finish;
+            SetFinish(finish);
         }
 
         if (other.TryGetComponent(out FailPlatform fail))
@@ -67,7 +67,7 @@ public class Ball : MonoBehaviour
     {
         if (_currentFinish == null && other.TryGetComponent(out Finish finish))
         {
-            _currentFinish = finish;
+            SetFinish(finish);
         }
     }
 
@@ -75,8 +75,7 @@ public class Ball : MonoBehaviour
     {
         if (other.TryGetComponent(out Finish finish) && _currentFinish == finish)
         {
-            _currentFinish = null;
-            IsFinished = false;
+            ResetFinish();
         }
     }
 
@@ -84,5 +83,47 @@ public class Ball : MonoBehaviour
     {
         if (_trailRenderer.enabled == false && other.collider.TryGetComponent(out Ball ball) == false)
             _trailRenderer.enabled = true;
+    }
+
+    private void ResetFinish()
+    {
+        _currentFinish = null;
+        IsFinished = false;
+
+        if (_ripple != null)
+            StopCoroutine(_ripple);
+
+        transform.localScale = _startScale;
+    }
+
+    private void SetFinish(Finish finish)
+    {
+        _currentFinish = finish;
+
+        _ripple = StartCoroutine(Ripple());
+    }
+
+    private IEnumerator Ripple()
+    {
+        float duration = 0.4f;
+        float lostTime = 0;
+        Vector3 endScale = _startScale * 1.1f;
+
+        while (true)
+        {
+            while (lostTime < 1)
+            {
+                lostTime += Time.deltaTime / duration;
+                transform.localScale = Vector3.Lerp(_startScale, endScale, lostTime);
+                yield return null;
+            }
+
+            while (lostTime > 0)
+            {
+                lostTime -= Time.deltaTime / duration;
+                transform.localScale = Vector3.Lerp(_startScale, endScale, lostTime);
+                yield return null;
+            }
+        }
     }
 }
