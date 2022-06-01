b2bdc12f5acb5552120c977ef532c0202a199864
Inheo <agch777505@gmail.com>
2022-06-01 12:30:49 +0300

    added rotator component

---
+++ Assets/Scripts/Obstacle/Rotator.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] private Vector3 _rotateEuler = Vector3.up;
+    [SerializeField] private float _speed = 5;
+
+    private void Update()
+    {
+        transform.Rotate(_rotateEuler * _speed);
+    }
+}
