45ced7ec271fce954dc332e2c49f87e3d803788a
Inheo <agch777505@gmail.com>
2022-06-02 10:18:28 +0300

    created component fall emitter implemented to abstract fall emitter

---
+++ Assets/Scripts/Ball/Fall/Emitters/FallEmitter.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class FallEmitter : AbstracFallEmitter
+{
+    [SerializeField] private LayerMask _platformLayer;
+    [SerializeField] private TrailRenderer _trailRenderer;
+    [SerializeField] private Rigidbody _rigidbody;
+
+    private readonly float _delay = 0.3f;
+    private float _lostTime = 0;
+
+    public override event System.Action OnFalled;
+
+    private void Update()
+    {
+        if(IsFalled() == true)
+        {
+            _lostTime = 0;
+            _trailRenderer.emitting = false;
+
+            OnFalled?.Invoke();  
+        }
+
+        _lostTime += Time.deltaTime;
+    }
+
+    protected override bool IsFalled() => _lostTime > _delay && _rigidbody.velocity.y < -20 && Physics.Linecast(transform.position, Vector3.down * 2f + transform.position, _platformLayer);
+}
