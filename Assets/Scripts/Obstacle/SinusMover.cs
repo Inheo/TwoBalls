5971fb2854a6f69891bf219285a68167c5ef5f9c
Inheo <agch777505@gmail.com>
2022-06-03 10:28:00 +0300

    added the ability to move along the x-axis and from any point

--- Assets/Scripts/Obstacle/SinusMover.cs
+++ Assets/Scripts/Obstacle/SinusMover.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 
 public class SinusMover : MonoBehaviour
 {
-    [SerializeField] private float _speed = 1f;
+    [SerializeField] private float _ySpeed = 1f;
+    [SerializeField] private float _xSpeed = 1f;
     [SerializeField] private float _amplitude = 1f;
+
+    private Vector3 _startPosition;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+    }
 
     private void Update()
     {
-        float y = Mathf.Sin(Time.time * _speed) * _amplitude;
+        float y = Mathf.Sin(Time.time * _ySpeed) * _amplitude;
+        float x = Mathf.Cos(Time.time * _xSpeed) * _amplitude;
 
-        transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        transform.position = new Vector3(_startPosition.x + x, _startPosition.y + y, transform.position.z);
     }
 }
