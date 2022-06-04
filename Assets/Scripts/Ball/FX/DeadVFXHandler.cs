d373129d113282d3fd7980e6d7b0179ce4761bb0
Inheo <agch777505@gmail.com>
2022-06-04 20:56:20 +0300

    created dead vfx handler component

---
+++ Assets/Scripts/Ball/FX/DeadVFXHandler.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Ball))]
+public class DeadVFXHandler : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _vfxPrefab;
+
+    private Ball _ball;
+
+    private void Awake()
+    {
+        _ball.GetComponent<Ball>();
+    }
+
+    private void Start()
+    {
+        _ball.OnFail += Play;
+    }
+
+    private void OnDestroy()
+    {
+        _ball.OnFail -= Play;
+    }
+
+    private void Play()
+    {
+        Instantiate(_vfxPrefab, transform.position, Quaternion.identity);
+    }
+}
