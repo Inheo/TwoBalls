8bb933fe2eeaf4eddbe3ed2571362a077cea1d57
Inheo <agch777505@gmail.com>
2022-06-07 12:31:54 +0300

    added vfx when collision with spring

--- Assets/Scripts/Obstacle/Spring.cs
+++ Assets/Scripts/Obstacle/Spring.cs
@@ -3,12 +3,24 @@ using UnityEngine;
 public class Spring : MonoBehaviour
 {
     [SerializeField] private float _force = 50;
+    [SerializeField] private ParticleSystem _jumpParticlePrefab;
 
     private void OnCollisionEnter(Collision other)
     {
         if(other.rigidbody != null)
         {
             other.rigidbody.AddForce(_force * Vector3.up, ForceMode.Impulse);
+
+            SpawnVFX(other.transform.position);
         }
+    }
+
+    private void SpawnVFX(Vector3 position)
+    {
+        var vfx = Instantiate(_jumpParticlePrefab, position, Quaternion.identity);
+        vfx.transform.localScale = Vector3.one * 2;
+        vfx.Play();
+        ParticleSystem.MainModule mainVfx = vfx.main;
+        mainVfx.stopAction = ParticleSystemStopAction.Destroy;
     }
 }
